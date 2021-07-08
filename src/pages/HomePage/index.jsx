33e3e2aac680ec8bd0c422c1da3595d6d4443623
NhatKhanh <nhatkhanh9a3@gmail.com>
2021-07-08 12:58:58 +0700

    add home page

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,43 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import HeaderHome from '../../components/HeaderHome';
+import { GlobalActions } from '../../redux/rootAction';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import SidebarHome from '../../components/SidebarHome';
 
+const { Header, Content, Footer, Sider } = Layout;
 function HomePage(props) {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+  // fake loading
+  useEffect(() => {
+    dispatch(GlobalActions.setIsLoading(true));
+    const loadingFake = setTimeout(() => {
+      dispatch(GlobalActions.setIsLoading(false));
+    }, 500);
+    return () => {
+      clearTimeout(loadingFake);
+    };
+  });
+  return (
+    <div className="container">
+      {/* <Header /> */}
+      <Layout>
+        <HeaderHome />
+        <Layout>
+          <SidebarHome />
+          <Content style={{ padding: '0 50px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+              <Breadcrumb.Item>Covid</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ minHeight: '280px', padding: '24px', background: '#fff' }}>Content</div>
+          </Content>
+        </Layout>
+        <Footer>Footer</Footer>
+      </Layout>
+    </div>
+  );
 }
 
 export default HomePage;
--- src/pages/NewsPage/components/HeaderNews/index.jsx
+++ src/pages/NewsPage/components/HeaderNews/index.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import styles from './HeaderNews.module.css';
 import { MenuOutlined, SearchOutlined, UserOutlined } from '@ant-design/icons';
+import { useHistory } from 'react-router';
 
 function HeaderNews(props) {
+  const history = useHistory();
   return (
     <header className={styles.headerContainer}>
-      <div className={styles.iconWrapper}>
-        <svg
-          className={styles.icon}
-          xmlns="http://www.w3.org/2000/svg"
-          width="40"
-          height="40"
-          viewBox="0 0 32 32"
-        >
-          <g
-            transform="matrix(.05696 0 0 .05696 .647744 2.43826)"
-            fill="none"
-            fillRule="evenodd"
-          >
+      <div className={styles.iconWrapper} onClick={() => history.push('/')}>
+        <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 32 32">
+          <g transform="matrix(.05696 0 0 .05696 .647744 2.43826)" fill="none" fillRule="evenodd">
             <circle r="50.167" cy="237.628" cx="269.529" fill="#00d8ff" />
             <g stroke="#00d8ff" strokeWidth="24">
               <path d="M269.53 135.628c67.356 0 129.928 9.665 177.107 25.907 56.844 19.57 91.794 49.233 91.794 76.093 0 27.99-37.04 59.503-98.083 79.728-46.15 15.29-106.88 23.272-170.818 23.272-65.554 0-127.63-7.492-174.3-23.44-59.046-20.182-94.61-52.103-94.61-79.56 0-26.642 33.37-56.076 89.415-75.616 47.355-16.51 111.472-26.384 179.486-26.384z" />
